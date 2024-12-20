fb103c04705f6b8fb5928de8b4e0a9a8c61f6cf0
KhaledMansoor14 <Khaledmansoor14@gmail.com>
2024-12-20 13:45:47 +0200

    Best Seller Item Done

--- lib/features/home/presentation/views/widgets/best_seller_list_view_item.dart
+++ lib/features/home/presentation/views/widgets/best_seller_list_view_item.dart
@@ -1,7 +1,9 @@
 
 import 'package:bookly/core/assets.dart';
 import 'package:bookly/core/styles.dart';
+import 'package:bookly/features/home/presentation/views/widgets/book_rating.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class BestSellerListViewItem extends StatelessWidget {
   const BestSellerListViewItem({super.key});
@@ -30,26 +32,30 @@ class BestSellerListViewItem extends StatelessWidget {
               ),
             ),
             SizedBox(width: 30),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-              SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.5,
-                  child: Text(
-                    'Harry Potter and the Goblet of Fire',
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
-                    style: Styles.textStyle20,
-                  )),
-              const SizedBox(height: 3,),
-              Text('J.K. Rowling',
-              style: Styles.textStyle14,),
-              Row(
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text('19.99\$', style: Styles.textStyle20,)
-                ],
-              )
-            ])
+                SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.5,
+                    child: Text(
+                      'Harry Potter and the Goblet of Fire',
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                      style: Styles.textStyle20,
+                    )),
+                const SizedBox(height: 3,),
+                Text('J.K. Rowling',
+                style: Styles.textStyle14,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('19.99\$', style: Styles.textStyle20,),
+                    BookRating(),
+                  ],
+                )
+              ]),
+            )
           ],
         ),
       ),
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -5,6 +5,7 @@ import 'package:bookly/features/home/presentation/views/widgets/best_seller_list
 import 'package:bookly/features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:bookly/features/home/presentation/views/widgets/featrued_list_view.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
