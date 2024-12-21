94686a9d311980df43ecdc01e9ea946008d47378
KhaledMansoor14 <Khaledmansoor14@gmail.com>
2024-12-21 09:12:51 +0200

    add custom book item  to  book details view

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,32 +1,25 @@
+import 'package:bookly/features/home/presentation/views/widgets/custom_book_details_app_bar.dart';
+import 'package:bookly/features/home/presentation/views/widgets/custom_list_view_item.dart';
+import 'package:bookly/features/home/presentation/views/widgets/featrued_list_view.dart';
 import 'package:flutter/material.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
   const BookDetailsViewBody({super.key});
-
   @override
   Widget build(BuildContext context) {
+  var fullWidth = MediaQuery.of(context).size.width;
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 6),
       child: Column(
-        children: const [
-          CustomBookDetailsAppBar()
+        children: [
+          CustomBookDetailsAppBar(),
+          Padding(
+            padding: EdgeInsets.symmetric(
+                horizontal: fullWidth * 0.2),
+            child: const FeatruredListViewItem(),
+          ),
         ],
       ),
-    );
-  }
-}
-
-class CustomBookDetailsAppBar extends StatelessWidget {
-  const CustomBookDetailsAppBar({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        IconButton(onPressed: (){}, icon: const Icon(Icons.close)),
-        IconButton(onPressed: (){}, icon: const Icon(Icons.shopping_cart_outlined))
-      ],
     );
   }
 }
--- lib/features/home/presentation/views/widgets/custom_book_details_app_bar.dart
+++ lib/features/home/presentation/views/widgets/custom_book_details_app_bar.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 
 class CustomBookDetailsAppBar extends StatelessWidget {
-  const CustomBookDetailsAppBar ({super.key});
+  const CustomBookDetailsAppBar({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        IconButton(onPressed: (){}, icon: const Icon(Icons.close)),
+        IconButton(onPressed: (){}, icon: const Icon(Icons.shopping_cart_outlined))
+      ],
+    );
   }
 }
