d659e72271f7e831673cfaf87800dd02e421f550
KhaledMansoor14 <Khaledmansoor14@gmail.com>
2024-12-22 08:55:20 +0200

    Refactior Books details View

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,8 +1,5 @@
-import 'package:bookly/core/utils/styles.dart';
-import 'package:bookly/features/home/presentation/views/widgets/book_rating.dart';
-import 'package:bookly/features/home/presentation/views/widgets/books_action.dart';
+import 'package:bookly/features/home/presentation/views/widgets/books_detail_section.dart';
 import 'package:bookly/features/home/presentation/views/widgets/custom_book_details_app_bar.dart';
-import 'package:bookly/features/home/presentation/views/widgets/custom_list_view_item.dart';
 import 'package:bookly/features/home/presentation/views/widgets/similer_books_list_view.dart';
 import 'package:flutter/material.dart';
 
@@ -10,7 +7,6 @@ class BookDetailsViewBody extends StatelessWidget {
   const BookDetailsViewBody({super.key});
   @override
   Widget build(BuildContext context) {
-    var fullWidth = MediaQuery.of(context).size.width;
     return CustomScrollView(
       slivers: [
         SliverFillRemaining(
@@ -19,50 +15,11 @@ class BookDetailsViewBody extends StatelessWidget {
             padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 6),
             child: Column(
               children: [
-                CustomBookDetailsAppBar(),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: fullWidth * 0.22),
-                  child: const FeatruredListViewItem(),
-                ),
-                const SizedBox(height: 43),
-                Text('The Jungle Book',
-                    style: Styles.textStyle30.copyWith(
-                      fontWeight: FontWeight.w600,
-                    )),
-                const SizedBox(height: 6),
-                Opacity(
-                  opacity: .7,
-                  child: Text(
-                    'Rudyard Kipling',
-                    style: Styles.textStyle18.copyWith(
-                      fontStyle: FontStyle.italic,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                ),
-                SizedBox(height: 18),
-                BookRating(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                ),
-                const SizedBox(height: 37),
-                const BookAction(),
+                const CustomBookDetailsAppBar(),
+                const BookDetailsSection(),
                 const Expanded(child: SizedBox(height: 50)),
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    'You can also like',
-                    style: Styles.textStyle16.copyWith(
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: 16,
-                ),
-                const SimilarBooksListView(),
-                SizedBox(
-                  height: 40,
-                ),
+                SimilarBooksListView(),
+                SizedBox(height: 40),
               ],
             ),
           ),
---
+++ lib/features/home/presentation/views/widgets/similer_books_section.dart
@@ -0,0 +1,28 @@
+
+
+import 'package:bookly/core/utils/styles.dart';
+import 'package:bookly/features/home/presentation/views/widgets/similer_books_list_view.dart';
+import 'package:flutter/material.dart';
+
+class SimilerBooksSection extends StatelessWidget {
+  const SimilerBooksSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'You can also like',
+          style: Styles.textStyle16.copyWith(
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        SizedBox(
+          height: 16,
+        ),
+        const SimilarBooksListView(),
+      ],
+    );
+  }
+}
