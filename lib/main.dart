12689f5be17586e8a44ace148d6820ab21f2bf29
KhaledMansoor14 <Khaledmansoor14@gmail.com>
2024-12-23 07:42:32 +0200

    Create home repo

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:bookly/constant.dart';
 import 'package:bookly/core/utils/app_router.dart';
-import 'package:bookly/features/splash/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:go_router/go_router.dart';
 
 void main() {
   runApp(const Bookly());
