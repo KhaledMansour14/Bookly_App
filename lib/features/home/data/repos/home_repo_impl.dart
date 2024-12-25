033e7a8cdfdc0af43bde80972686a4663db7b4fa
KhaledMansoor14 <Khaledmansoor14@gmail.com>
2024-12-25 10:58:08 +0200

    create featured box emit

--- lib/features/home/data/repos/home_repo_impl.dart
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -30,7 +30,21 @@ class HomeRepoImpl implements HomeRepo {
   }
 
   @override
-  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() {
-    throw UnimplementedError();
-  }
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() async {
+try {
+      var data = await appService.get(
+          endPoint: 'volumes?q=Programming&filter=free-ebooks');
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+
+      return right(books);
+    } catch (e) {
+      if (e is DioError) {
+        return left(ServerFalilure.fromDioError(e));
+      }
+
+      return left(ServerFalilure(errMessage: e.toString()));
+    }  }
 }
